edf3b89509ca443f546bf4222bd6248206c5a1e1
Dean Lozo <dean.lozo@gmail.com>
2016-01-19 20:56:09 +0100

    Fixed typo and implemented sort

---
+++ web/static/js/sortEntities.js
@@ -0,0 +1,17 @@
+const selectSort = document.querySelector("#sort");
+
+const sortEntities = () => {
+  const sortvalue = selectSort.value;
+  const entityList = document.querySelector(".entity-list")
+  const entity = document.querySelectorAll(".entity");
+  const entityArray = Array.prototype.slice.call(entity);
+  entityArray.sort((a, b) =>
+    a.querySelector(`.${sortvalue}`).textContent.localeCompare(b.querySelector(`.${sortvalue}`).textContent))
+    .forEach(function (item) {
+      entityList.appendChild(item);
+    });
+};
+
+selectSort.addEventListener("change", sortEntities);
+
+export default sortEntities;
